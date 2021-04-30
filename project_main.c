a85b8099e1b76af25b7c457c2a0f763710b0b1dc
amansurya <40491650+amansurya@users.noreply.github.com>
2021-04-30 16:58:58 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,32 +1,25 @@
+#include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
+#include "activity4.h"
 
-
-#include <avr/io.h>
-#include<util/delay.h>
-int main(void)
+int main()
 {
-DDRB |=(1<<PB0);//SET B0=1 FOR LED
-    // Insert code
-DDRD&=~(1<<PD0);//CLEAR BIT
-PORTD|=(1<<PD0);
-DDRD&=~(1<<PD1);//CLEAR BIT
-PORTD|=(1<<PD1);
-    while(1){
-            if((!(PIND&(1<<PD0)))&&(!(PIND&(1<<PD1)))){
-                PORTB |=(1<<PB0);
 
-                //_delay_ms(2000);
-            }
-            else{
-                 PORTB&=~(1<<PB0);
-                 //_delay_ms(2000);
-            }
+uint16_t t1;
+int t2;
+while(1)
+{
+int a1;
+a1=activity1();
 
-    }
-/**
-* If the person is seated, the button sensor D0 will detect it and will be ON if seated.
-* If we want the temperature sensor to be activated then button D1 will detect it and if both are 1 , the temperature sensor (represented by LED) will turn ON.
-*/
+if(a1==1)
+{
+t1=activity2();
+t2=activity3(t1);
+activity4(t2);
 
+}
 
-    return 0;
+}
 }
