5e203fea446bd28c120372fab2464b65636a2da3
amansurya <40491650+amansurya@users.noreply.github.com>
2021-04-30 20:10:31 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -8,32 +8,32 @@
  * @copyright Copyright (c) 2021
  * 
  */
-#ifndef Activity3h
-#define Activity3h
+#ifndef Activity3H
+#define Activity3H
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 /**
- * @brief macro for temperature condtition 1
+ * @brief macro for temperature condition 1
  * 
  */
 #define Cond1Temp (temp>=0)&&(temp<=200)
 
 /**
- * @brief macro for temperature condtition 2
+ * @brief macro for temperature condition 2
  * 
  */
 #define Cond2Temp (temp>=210)&&(temp<=500)
 
 /**
- * @brief macro for temperature condtition 3
+ * @brief macro for temperature condition 3
  * 
  */
 #define Cond3Temp (temp>=510)&&(temp<=700)
 
 /**
- * @brief macro for temperature condtition 4
+ * @brief macro for temperature condition 4
  * 
  */
 #define Cond4Temp (temp>=710)&&(temp<=1024)
